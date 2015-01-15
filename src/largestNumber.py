cdcdc4e8478ecdeca55f76cd6e8331f18cc7f26c
zhyu <angellwings@gmail.com>
2015-01-15 17:03:04 +0900

    add solution for Largest Number

---
+++ src/largestNumber.py
@@ -0,0 +1,9 @@
+class Solution:
+    # @param num, a list of integers
+    # @return a string
+    def largestNumber(self, num):
+        return ''.join(sorted(map(str, num), cmp=self.cmp)).lstrip('0') or '0'
+
+    def cmp(self, x, y):
+        return [1, -1][x + y > y + x]
+
