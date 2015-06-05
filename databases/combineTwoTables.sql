37b74310892b4bbcd0c37a7101ded6f9f16f7fa6
zhyu <angellwings@gmail.com>
2015-06-05 12:34:30 +0900

    remove the hinting comments from sql file

--- databases/combineTwoTables.sql
+++ databases/combineTwoTables.sql
@@ -1,2 +1,1 @@
-# Write your MySQL query statement below
 select FirstName, LastName, City, State from Person left join Address on Person.PersonId=Address.PersonId;
--- databases/secondHighestSalary.sql
+++ databases/secondHighestSalary.sql
@@ -1,2 +1,1 @@
-# Write your MySQL query statement below
 select max(Salary) from Employee where Salary < (select max(Salary) from Employee);
